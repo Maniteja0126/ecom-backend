85c87bc38ee340713442a6be32b874191f47e232
Maniteja0126 <maniteja2601@gmail.com>
2023-08-16 13:43:49 +0530

    added products

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const products = require('./db/products')
+const app=express()
+app.use(express.json())
+app.get('/',(req,res) =>{
+    res.send('Hello from Express')
+})
+app.get("/api/products" , (req,res)=>{
+    res.status(200).json(products)
+})
+app.listen(3000, ()=>{
+    console.log("server is up and running")
+})
