c6438d97d969e1a0e1a6715b348de80a53adfe01
Maniteja0126 <maniteja2601@gmail.com>
2023-08-16 14:09:03 +0530

    fix: fixed cors issue

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express')
+const cors = require('cors')
 const products = require('./db/products')
 const app=express()
+app.use(cors())
 app.use(express.json())
+
 app.get('/',(req,res) =>{
     res.send('Hello from Express')
 })
+
 app.get("/api/products" , (req,res)=>{
     res.status(200).json(products)
 })
+
 app.listen(3000, ()=>{
     console.log("server is up and running")
 })
